cb146b899b66385075ac3e68bbda80df04d002b5
Denky <danielamaral.f@hotmail.com>
2023-01-19 17:48:34 -0300

    fim do dia

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ class AgeApp(MDApp):
 
     def __init__(self, token, host, **kwargs):
         super().__init__(**kwargs)
-        self.__version__ = "0.0.10.15"
+        self.__version__ = "0.0.10.21"
         self.token = token
         self.manager = ScreenManager()
         self.DEBUG = False
